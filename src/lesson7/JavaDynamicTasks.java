f58141efcf4ba78c41df1a5db03272398fd36b8a
Artyom_Sidorov_3530901/90004 <artyom.sidoroff2014@yandex.ru>
2020-10-31 17:19:57 +0300

    lesson7

--- src/lesson7/JavaDynamicTasks.java
+++ src/lesson7/JavaDynamicTasks.java
@@ -2,6 +2,7 @@ package lesson7;
 
 import kotlin.NotImplementedError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -17,9 +18,37 @@ public class JavaDynamicTasks {
      * Если общей подпоследовательности нет, вернуть пустую строку.
      * Если есть несколько самых длинных общих подпоследовательностей, вернуть любую из них.
      * При сравнении подстрок, регистр символов *имеет* значение.
+     * время - O(N*M) Память - O(N*M)
      */
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int n = first.length();
+        int m = second.length();
+        int[][] f = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1))
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                else
+                    f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        int i = n;
+        int j = m;
+        while (i > 0 && j > 0) {
+            if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                res.append(first.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (f[i - 1][j] == f[i][j])
+                    i--;
+                else
+                    j--;
+            }
+        }
+        res.reverse();
+        return res.toString();
     }
 
     /**
@@ -33,9 +62,40 @@ public class JavaDynamicTasks {
      * Если самых длинных возрастающих подпоследовательностей несколько (как в примере),
      * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
+     *
+     * время - O(N*logN) Память - O(N)
      */
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
-        throw new NotImplementedError();
+        int[] p = new int[list.size()];
+        int[] m = new int[list.size() + 1];
+        int l = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int lo = 1;
+            int hi = l;
+        while (lo <= hi){
+                int mid = (int) Math.ceil(((lo + hi) / 2));
+                if (list.get(m[mid]) <= list.get(i)) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        int newL = lo;
+        p[i] = m[newL - 1];
+        m[newL] = i;
+        if (newL > l)
+            l = newL;
+        }
+        int[] s = new int[l];
+        int k = m[l];
+        for (int i = l - 1; i >= 0; i-- ) {
+            s[i] = list.get(k);
+            k = p[k];
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = s.length - 1; i >= 0; i--)
+            res.add(s[i]);
+        return res;
     }
 
     /**
